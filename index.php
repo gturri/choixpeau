ce19d6af8fd38b0eaeb5bb57ea056abf8a5d5222
Guillaume Turri <guillaume.turri@gmail.com>
2020-11-15 22:59:20 +0100

    Attempt to add the img and the speech balloon
    The image is taken from google image with filter Creative Common.
    (The whole stuff is pretty lame, but that's a first iteration).

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 <head>
   <title>Choixpeau</title>
   <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+<div id="outter">
+<img id="choixpeau" class="col" src="choixpeau.jpg" alt="choixpeau" />
+<div class="small_col"></div>
+<div id="talkbubble" class="col">
 <?php
 require_once "lib.php";
 $eleve = $_REQUEST["eleve"];
@@ -19,12 +25,15 @@ if (isset($eleve)) {
 	echo "</div>";
 }
 ?>
-
-<div>"Qui veux-tu que j'évalue maintenant ?"</div>
+  
+  <div>Qui veux-tu que j'évalue maintenant ?</div>
+</div>
+</div>
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 	<input type="text" name="eleve">
   <input type="submit" value="go">
 </form>
+
 
 </body>
 </html>
