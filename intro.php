59c4a6d7a9e3bbb2ecdfe24b274a9d24872b775b
Guillaume Turri <guillaume.turri@gmail.com>
2020-11-21 21:49:46 +0100

    Add intro.php
    This adds the feature that the introduction page should let the choixpeau talk

---
+++ intro.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>Choixpeau</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<audio onEnded="window.location.href='index.php'" id="audio_play" src="choixpeau.m4a">Your browser does not support the <code>audio</code> element</audio>
+<img onClick="document.getElementById('audio_play').play(); return false;" id="choixpeau" class="col" src="choixpeau.jpg" alt="choixpeau" />
+
+
