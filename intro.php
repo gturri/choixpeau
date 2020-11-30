b64c5c67a3be8afec5a05d641531a6f1322c58aa
Guillaume Turri <guillaume.turri@gmail.com>
2020-11-30 22:27:10 +0100

    Intro reads the full poem and say "bravo"
    - the full poem is read
    - the user can click once the students are done with the poem
      in order to go to the next page

--- intro.php
+++ intro.php
@@ -3,11 +3,28 @@
   <title>Choixpeau</title>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="style.css">
+  <script>
+var alreadyPlayedPoem = false;
+var audioIsBeingPlayed = false;
+function choixpeauClicked(){
+  if (window.audioIsBeingPlayed){
+    return;
+  }
+  window.audioIsBeingPlayed = true;
+  if ( !window.alreadyPlayedPoem ){
+    document.getElementById('audio_poeme').play();
+    window.alreadyPlayedPoem = true;
+  } else {
+    document.getElementById('audio_bravo').play();
+  }
+}
+  </script>
 </head>
 <body>
 
 
-<audio onEnded="window.location.href='index.php'" id="audio_play" src="choixpeau.m4a">Your browser does not support the <code>audio</code> element</audio>
-<img style="cursor:pointer;" onClick="document.getElementById('audio_play').play(); return false;" id="choixpeau" class="col" src="choixpeau.jpg" alt="choixpeau" />
+<audio onEnded="window.audioIsBeingPlayed=false;" id="audio_poeme" src="poeme.m4a">Your browser does not support the audio element</audio>
+<audio onEnded="window.location.href='index.php'" id="audio_bravo" src="bravo.m4a">Your browser does not support the audio element</audio>
+<img style="cursor:pointer;" onClick="choixpeauClicked();" id="choixpeau" class="col" src="choixpeau.jpg" alt="choixpeau" />
 
 
