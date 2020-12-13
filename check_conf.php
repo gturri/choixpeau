9745cd7a6d8c3383cfb3788cbd565b7807c614b2
Guillaume Turri <guillaume.turri@gmail.com>
2020-12-13 23:04:04 +0100

    Add blasons in conf
    the next step will be to use them in the dialogues

--- check_conf.php
+++ check_conf.php
@@ -17,12 +17,28 @@ if (!array_key_exists("maisons", $conf)) {
   close_and_exit();
 }
 
+// Check the blasons are correctly defined
+$has_error = false;
+if (!array_key_exists("blason", $conf)) {
+  echo "La liste des blasons n'est pas définie.";
+  close_and_exit();
+}
+
+foreach($conf["maisons"] as $maison){
+  if (!array_key_exists($maison, $conf["blason"])){
+    echo "la maison $maison n'a pas de blason<br>";
+    $has_error = true;
+  } else if (!file_exists($conf["blason"][$maison])) {
+     echo "le fichier " . $conf["blason"][$maison] . " n'a pas l'air d'exister<br>";
+     $has_error = true;
+  }
+}
+
 // Check each maison has a list of eleves (even if the list is empty)
 if (!array_key_exists("eleves", $conf)) {
   echo "Les listes d'élèves ne sont pas définies.";
   close_and_exit();
 }
-$has_error = false;
 foreach ($conf["maisons"] as $maison){
 	if (!array_key_exists($maison, $conf["eleves"])){
 		echo "la maison $maison n'a pas de liste d'élèves.<br>";
@@ -116,6 +132,15 @@ if (!array_key_exists("msg_erreur", $conf)) {
 		}
 		echo "</ul>";
 	}
+?>
+</ul>
+
+<h1>Liste des blasons</h1>
+<ul>
+<?php
+  foreach(get_maisons() as $maison){
+    echo "<li>$maison : <img src=" . $conf["blason"][$maison] . " /></li>";
+  }
 ?>
 </ul>
 
--- config.php
+++ config.php
@@ -3,9 +3,16 @@
 // Indiquer la liste des maisons susceptibles d'être retournées 
 $conf["maisons"] = array(
 	"serpentard"
-	, "griffon d'or"
-	, "serre d'aigle"
+	, "gryffondor"
+	, "serdaigle"
+    , "poufsouffle"
 );
+
+// Indiquer le nom du fichier du blason
+$conf["blason"]["serpentard"] = "serpentard.png";
+$conf["blason"]["gryffondor"] = "gryffondor.png";
+$conf["blason"]["poufsouffle"] = "poufsouffle.png";
+$conf["blason"]["serdaigle"] = "serdaigle.png";
 
 // Saisir la liste des élèves par maison
 $conf["eleves"]["serpentard"] = array(
@@ -13,26 +20,30 @@ $conf["eleves"]["serpentard"] = array(
 	, "fifi"
 	, "loulou"
 );
-$conf["eleves"]["griffon d'or"] = array(
+$conf["eleves"]["gryffondor"] = array(
 	"pim"
 	, "pam"
 );
-$conf["eleves"]["serre d'aigle"] = array(
+$conf["eleves"]["serdaigle"] = array(
 	"aéris"
 	, "loïc"
 );
+$conf["eleves"]["poufsouffle"] = array();
 
 // Saisir la liste des phrases par maison
 $conf["phrases"]["serpentard"] = array(
 	"tu as l'air jeune et dynamique"
 	, "tu es très charismatique"
 );
-$conf["phrases"]["griffon d'or"] = array(
+$conf["phrases"]["gryffondor"] = array(
 	"tu as l'air très droitier"
 	, "tu es très grand"
 );
-$conf["phrases"]["serre d'aigle"] = array(
+$conf["phrases"]["serdaigle"] = array(
 	"tu es très svelte"
+);
+$conf["phrases"]["poufsouffle"] = array(
+    "vive les souffles"
 );
 
 // Saisir le message d'erreur à retourner si le nom de l'élève n'est pas trouvé
