20f601f8ed6dbdd79523bcbe0ddbf209ac24fd8d
Guillaume Turri <guillaume.turri@gmail.com>
2020-12-13 22:09:21 +0100

    Add intro escape-game style

--- intro.php
+++ intro.php
@@ -18,13 +18,21 @@ function choixpeauClicked(){
     document.getElementById('audio_bravo').play();
   }
 }
+
+function next(idCurrent, idNext){
+  document.getElementById(idCurrent).classList.add('hide');
+  document.getElementById(idNext).classList.remove('hide');
+}
   </script>
 </head>
 <body>
 
+<img style="cursor:pointer;" onClick="next('chateau', 'salle');" id="chateau" src="chateau.png" alt="chateau.png" />
+<img class="hide" style="cursor:pointer;" onClick="next('salle', 'localisation');" id="salle" src="salle_principale.png" alt="salle principale" />
+<img class="hide" style="cursor:pointer;" onClick="next('localisation', 'choixpeau');" id="localisation" src="localisation_choixpeau.png" alt="localisation choixpeau" />
 
 <audio onEnded="window.audioIsBeingPlayed=false;" id="audio_poeme" src="poeme.m4a">Your browser does not support the audio element</audio>
 <audio onEnded="window.location.href='index.php'" id="audio_bravo" src="bravo.m4a">Your browser does not support the audio element</audio>
-<img style="cursor:pointer;" onClick="choixpeauClicked();" id="choixpeau" class="col" src="choixpeau2.png" alt="choixpeau" />
+<img class="hide" style="cursor:pointer;" onClick="choixpeauClicked();" id="choixpeau" class="col" src="choixpeau2.png" alt="choixpeau" />
 
 
