eef192e4d71e5b76dcd16830abb5339b114e1957
Guillaume Turri <guillaume.turri@gmail.com>
2020-12-14 19:28:16 +0100

    Intro img bigger and centered

--- intro.php
+++ intro.php
@@ -21,18 +21,21 @@ function choixpeauClicked(){
 
 function next(idCurrent, idNext){
   document.getElementById(idCurrent).classList.add('hide');
+  document.getElementById(idCurrent).classList.remove('block');
+
   document.getElementById(idNext).classList.remove('hide');
+  document.getElementById(idNext).classList.add('block');
 }
   </script>
 </head>
 <body>
 
-<img style="cursor:pointer;" onClick="next('chateau', 'salle');" id="chateau" src="chateau.png" alt="chateau.png" />
-<img class="hide" style="cursor:pointer;" onClick="next('salle', 'localisation');" id="salle" src="salle_principale.png" alt="salle principale" />
-<img class="hide" style="cursor:pointer;" onClick="next('localisation', 'choixpeau');" id="localisation" src="localisation_choixpeau.png" alt="localisation choixpeau" />
+<img class="img_intro block" onClick="next('chateau', 'salle');" id="chateau" src="chateau.png" alt="chateau.png" />
+<img class="img_intro hide" onClick="next('salle', 'localisation');" id="salle" src="salle_principale.png" alt="salle principale" />
+<img class="img_intro hide" onClick="next('localisation', 'choixpeau_intro');" id="localisation" src="localisation_choixpeau.png" alt="localisation choixpeau" />
 
+<img class="img_intro hide" onClick="choixpeauClicked();" id="choixpeau_intro" src="choixpeau2.png" alt="choixpeau" />
 <audio onEnded="window.audioIsBeingPlayed=false;" id="audio_poeme" src="poeme.m4a">Your browser does not support the audio element</audio>
 <audio onEnded="window.location.href='index.php'" id="audio_bravo" src="bravo.m4a">Your browser does not support the audio element</audio>
-<img class="hide" style="cursor:pointer;" onClick="choixpeauClicked();" id="choixpeau" class="col" src="choixpeau2.png" alt="choixpeau" />
 
 
