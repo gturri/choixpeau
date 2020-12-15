882929643ac4b56e9d6cc8bb3a4739b4c2b42389
Guillaume Turri <guillaume.turri@gmail.com>
2020-12-15 22:22:36 +0100

    Center choixpeau on main page
    it seems it will be easier to project

--- index.php
+++ index.php
@@ -7,8 +7,14 @@
 </head>
 <body>
 
-<div id="outter">
-<img id="choixpeau" class="col" src="choixpeau2.png" alt="choixpeau" />
+<div id="outter" class="centered">
+<div class="col">
+  <img id="choixpeau" src="choixpeau2.png" alt="choixpeau" />
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <input type="text" name="eleve">
+    <input type="submit" value="Nom de l'élève">
+  </form>
+</div>
 <div class="small_col"></div>
 <div id="talkbubble" class="col">
 <?php
@@ -32,10 +38,6 @@ if (isset($eleve)) {
   <div id="talk_4" <?php if ($displayed_eleve){echo "class=\"hide\"";} ?>>Qui veux-tu que j'évalue maintenant ?</div>
 </div>
 </div>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-	<input type="text" name="eleve">
-  <input type="submit" value="Nom de l'élève">
-</form>
 
 </body>
 </html>
